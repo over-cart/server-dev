b03ffaa5f38dc42ec1c162c85f183c5e6315b768
grachegrache <hoonse07@naver.com>
2016-09-21 02:53:36 +0900

    초기 화면 및 관리자 유저 분기
    admin 입력시 관리자 페이지
    그외 카트번호 입력시 유저 화면으로 이동

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,26 +1,35 @@
+# -*-coding:utf-8-*-
 # from app import app, socket_io
 from app import app
 from flask import render_template, jsonify, request
 from subprocess import call
 from flask_socketio import send
 from datetime import datetime
-
+from MySql_connector import connector
+import json
 
 @app.route('/')
 def index():
     return render_template('index-overcart.html')
 
-@app.route('/overcart/')
+@app.route('/overcart/', methods=['GET', 'POST'])
 def hello_world():
-    return "Hello OverCart Project Home Page!!"
+    if request.method == 'POST':
+        param_id = request.form['id']
+        print(param_id)
+        if param_id == 'admin':
+            tmp = render_template('admin-overcart.html')
+        else:
+            array_loc = [1,0,1,1,1,1, 0,1,0,0,1,0, 1,1,0,0,0,1, 1,0,1,0,0,1, 1,1,0,0,0,1, 0,1,1,1,0,0]
+            tmp = render_template('user-overcart.html', array_loc=json.dumps(array_loc))
+
+    return tmp
+
 
 @app.route('/overcart/chat')
 def chatting():
     return render_template('index-overcart.html')
 
-@app.route('/overcart/map')
-def shop_map():
-    return render_template('map-cell.html')
 
 # 카트의 움직임을 따라서 변화하는 정보를 받을 예정입니다 .
 @app.route('/overcart/trace', methods=['GET', 'POST'])
@@ -29,16 +38,20 @@ def trace_cart():
         return "This is GET type Request, <br>Please do POST type in JSON format... "
     elif request.method == 'POST':
         value = request.json
+        connector.insert_sign(value['rasp_id'],value['card_id'],value['datetime'])
         print(value)
+
         to_client = dict()
         to_client['response_time'] = datetime.now()
         to_client['rasp_id'] = value['rasp_id']
         to_client['card_id'] = value['card_id']
         return jsonify(to_client)
 
+
 @app.route('/data')
 def data():
-    return jsonify({'hello':'world', 'lee':'junsu'})
+    return jsonify({'hello': 'world', 'lee': 'junsu'})
+
 
 @app.route('/others')
 def others():
@@ -46,35 +59,21 @@ def others():
     print(value)
     return "other program run in background"
 
-@app.route('/admin', methods=['GET,POST'])
-def retrive():
-    if request.method == 'GET':
-        return "This is GET type Request, <br>Please do POST type in JSON format... "
-    elif request.method == 'POST':
-        value = request.json
-        print(value)
-        to_client = dict()
-        to_client['response_time'] = datetime.now()
-        to_client['rasp_id'] = value['rasp_id']
-        to_client['card_id'] = value['card_id']
-        return jsonify(to_client)
-
-
-# @socket_io.on("hello")
-# def hello(message):
-#     print(">>Hello : ", message)
-#     send(message, broadcast=True)
-#
-#
-# @socket_io.on("message")
-# def request(message):
-#     print("message : "+ message)
-#     to_client = dict()
-#     if message == 'new_connect':
-#         to_client['message'] = "새로운 유저가 난입하였다!!"
-#         to_client['type'] = 'connect'
-#     else:
-#         to_client['message'] = message
-#         to_client['type'] = 'normal'
-#     # emit("response", {'data': message['data'], 'username': session['username']}, broadcast=True)
-#     send(to_client, broadcast=True)
+    # @socket_io.on("hello")
+    # def hello(message):
+    #     print(">>Hello : ", message)
+    #     send(message, broadcast=True)
+    #
+    #
+    # @socket_io.on("message")
+    # def request(message):
+    #     print("message : "+ message)
+    #     to_client = dict()
+    #     if message == 'new_connect':
+    #         to_client['message'] = "새로운 유저가 난입하였다!!"
+    #         to_client['type'] = 'connect'
+    #     else:
+    #         to_client['message'] = message
+    #         to_client['type'] = 'normal'
+    #     # emit("response", {'data': message['data'], 'username': session['username']}, broadcast=True)
+    #     send(to_client, broadcast=True)
